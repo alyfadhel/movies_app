bb58b8333cc248fe3ace50cf55894295e33a32e8
alyfa <alyfadel67@gmail.com>
2022-12-21 14:54:26 +0200

    add movie model

---
+++ lib/features/movies/data/models/movie_model.dart
@@ -0,0 +1,30 @@
+import 'package:movies_clean_architecture/features/movies/domain/entities/movie.dart';
+
+class MovieModel extends Movie
+{
+  const MovieModel({
+    required super.id,
+    required super.title,
+    required super.backdropPath,
+    required super.genreIds,
+    required super.overview,
+    required super.releaseDate,
+    required super.voteAverage,
+  });
+
+  factory MovieModel.fromJson(Map<String,dynamic>json)
+  {
+    return MovieModel(
+        id: json['id'],
+        title: json['title'],
+        backdropPath: json['backdrop_path'],
+        genreIds: List<int>.from(json['genre_ids'].map((e) => e)),
+        overview: json['overview'],
+        releaseDate: json['release_date'],
+        voteAverage: json['vote_average'],
+    );
+  }
+
+
+
+}
